b714470e705866daab996ec0af11ee3b3e6aa7ad
Umesh Basnet <basnet0umesh@gmail.com>
2025-06-17 17:35:04 -0400

    Initial Commit

---
+++ WalkingBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WalkingBuddy
+//
+//  Created by Umesh Basnet on 2025-06-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WalkingBuddy/WalkingBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkingBuddyApp.swift
+//  WalkingBuddy
+//
+//  Created by Umesh Basnet on 2025-06-17.
+//
+
+import SwiftUI
+
+@main
+struct WalkingBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
